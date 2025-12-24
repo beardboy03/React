3c35b1db43bff15ae53306b11ef228fa110e6e7e
beardboy03 <kmr.sny913520@gmail.com>
2025-12-24 08:41:08 +0530

    EP-O1

---
+++ App.js
@@ -0,0 +1,36 @@
+/*
+<div id="parent">
+    <div id="child1">
+    <h1></h1>
+    </div>
+    <div id="child2">
+    <h1></h1>
+    </div>
+    <div>
+*/
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World From React!"
+// );
+const heading = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement(
+      "div",
+      { id: "child1" },
+      React.createElement("h1", {}, "Hello From Internal Child-1")
+    ),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      React.createElement("h1", {}, "Hello From Internal Child-2")
+    ),
+  ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+root.render(heading);
