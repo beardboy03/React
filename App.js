4f5be4920cb133ba84d7bfa4998886ea6a507948
beardboy03 <kmr.sny913520@gmail.com>
2025-12-27 14:36:52 +0530

    #

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 /*
 <div id="parent">
     <div id="child1">
@@ -24,6 +24,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(heading);
 root.render(heading);
 */
-const heading=React.createElement("h1",{id:"heading"},"Hello World!");
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+// const jsxHeading = <h1 id="heading">Using JSX</h1>;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+// React Element
+const heading=<h1 className="heading">Hello World!</h1>
+
+// React Component
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+ 
